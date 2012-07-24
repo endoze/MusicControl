33f604a42e5219500910962b2201cbd013e62d53
Endoze <endoze@endozemedia.com>
2012-07-24 07:58:30 -0400

    Initial Commit

---
+++ MusicControl/MusicControl.h
@@ -0,0 +1,13 @@
+//
+//  MusicControl.h
+//  MusicControl
+//
+//  Created by Endoze on 7/24/12.
+//  Copyright (c) 2012 EndozeMedia, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicControl : NSObject
+
+@end
---
+++ MusicControl/MusicControl.m
@@ -0,0 +1,13 @@
+//
+//  MusicControl.m
+//  MusicControl
+//
+//  Created by Endoze on 7/24/12.
+//  Copyright (c) 2012 EndozeMedia, LLC. All rights reserved.
+//
+
+#import "MusicControl.h"
+
+@implementation MusicControl
+
+@end

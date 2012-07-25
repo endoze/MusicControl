f303c267a15448bc217da2cf2238f841de6254bd
Endoze <endoze@endozemedia.com>
2012-07-24 22:07:18 -0400

    Add license header to files.  Add currentAlbum, currentArtwork, getPlayStatus, getPlayerPosition, getCurrentTrackDuration, and getPlayerPercentageComplete methods.

--- MusicControl/MusicControl.m
+++ MusicControl/MusicControl.m
@@ -3,15 +3,26 @@
 //  MusicControl
 //
 //  Created by Endoze on 7/24/12.
-//  Copyright (c) 2012 EndozeMedia, LLC. All rights reserved.
-//
-
+/*
+ Copyright (c) 2012 , EndozeMedia, LLC
+ All rights reserved.
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ Neither the name of the <ORGANIZATION> nor the names of its contributors may be used to endorse or promote products derived from this software
+ without specific prior written permission.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #import "MusicControl.h"
 #import "iTunes.h"
 #import "Spotify.h"
 #import "Rdio.h"
-
-
 
 #define kiTunesBundle @"com.apple.iTunes"
 #define kSpotifyBundle @"com.spotify.client"
@@ -151,7 +162,6 @@
             return [self.spotify soundVolume];
         case kRdio:
             return [self.rdio soundVolume];
-            
         default:
             return -1;
     }
@@ -168,7 +178,6 @@
             return [[self.spotify currentTrack] name];
         case kRdio:
             return [[self.rdio currentTrack] name];
-            
         default:
             return @"";
     }
@@ -185,12 +194,158 @@
             return [[self.spotify currentTrack] artist];
         case kRdio:
             return [[self.rdio currentTrack] artist];
-            
         default:
             return @"";
     }
 }
 
+- (NSString *)getCurrentAlbum
+{
+    switch (self.currentPlayer)
+    {
+        case kiTunes:
+            return [[self.iTunes currentTrack] album];
+            break;
+        case kSpotify:
+            return [[self.spotify currentTrack] album];
+        case kRdio:
+            return [[self.rdio currentTrack] album];
+        default:
+            return @"";
+    }
+}
+
+- (NSImage *)getCurrentArtwork
+{
+    NSData *artworkData;
+    NSImage *image;
+    
+    switch (self.currentPlayer)
+    {
+        case kiTunes:
+        {
+            // Array is populated with small, medium, large artwork
+            //TODO: Look at all images returned and grab biggest available artwork
+            artworkData = [[[[self.iTunes currentTrack] artworks] lastObject] rawData];
+            image = [[NSImage alloc] initWithData:artworkData];
+        }
+            break;
+        case kSpotify:
+        {
+            return [[self.spotify currentTrack] artwork];
+        }
+            break;
+        case kRdio:
+        {
+            artworkData = [[self.rdio currentTrack] artwork];
+            image = [[NSImage alloc] initWithData:artworkData];
+        }
+            break;
+        default:
+            break;
+            
+    }
+    return image;
+}
+
+- (NSString *)getPlayStatus
+{
+    switch (self.currentPlayer)
+    {
+        case kiTunes:
+        {
+            switch ([self.iTunes playerState])
+            {
+                case iTunesEPlSPaused:
+                    return @"Paused";
+                    break;
+                case iTunesEPlSPlaying:
+                    return @"Playing";
+                    break;
+                case iTunesEPlSStopped:
+                    return @"Stopped";
+                    break;
+                default:
+                    break;
+            }
+        }
+            break;
+        case kSpotify:
+        {
+            switch ([self.spotify playerState])
+            {
+                case SpotifyEPlSPaused:
+                    return @"Paused";
+                    break;
+                case SpotifyEPlSPlaying:
+                    return @"Playing";
+                    break;
+                case SpotifyEPlSStopped:
+                    return @"Stopped";
+                    break;
+                default:
+                    break;
+            }
+        }
+            break;
+        case kRdio:
+            switch ([self.rdio playerState])
+        {
+            case RdioEPSSPaused:
+                return @"Paused";
+                break;
+            case RdioEPSSPlaying:
+                return @"Playing";
+                break;
+            case RdioEPSSStopped:
+                return @"Stopped";
+                break;
+            default:
+                break;
+        }   
+        default:
+            return @"";
+    }
+    return @"";
+}
+
+- (double)getPlayerPosition
+{
+    switch (self.currentPlayer)
+    {
+        case kiTunes:
+            return [self.iTunes playerPosition];
+        case kSpotify:
+            return [self.spotify playerPosition];
+        case kRdio:
+            return [self.rdio playerPosition];
+        default:
+            return -1;
+    }
+}
+
+- (double)getCurrentTrackDuration
+{
+    switch (self.currentPlayer)
+    {
+        case kiTunes:
+            return [self.iTunes.currentTrack duration];
+        case kSpotify:
+            return [self.spotify.currentTrack duration];
+        case kRdio:
+            return [self.rdio.currentTrack duration];
+        default:
+            return -1;
+    }
+}
+
+- (double)getPlayerPercentageComplete
+{
+    double position = [self getPlayerPosition];
+    double duration = [self getCurrentTrackDuration];
+    
+    return position / duration;
+}
 
 
 @end
